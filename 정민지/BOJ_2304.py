4fd0656213fb4266521c9df8b4b25ca011e238fe
jellyKKing <wjdalswl0731@gmail.com>
2023-03-20 00:31:31 +0900

    [PGS] 호텔대실 / level2 / 120분
    https://school.programmers.co.kr/learn/courses/30/lessons/155651

--- 정민지/BOJ_2304.py
+++ 정민지/BOJ_2304.py
@@ -1,35 +1,27 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-maxVal, maxIdx = 0, 0
-startIdx, endIdx, arrEndIdx = 1001, 0, 0
-for i in range(N):
-    if maxVal < arr[i][1]:
-        maxVal = arr[i][1]
-        maxIdx = arr[i][0]
+arr.sort()
+col_arr = [i[1] for i in arr]
+maxVal = max(col_arr)
+arrMaxIdx = col_arr.index(maxVal)
 
-    if endIdx < arr[i][0]:
-        endIdx = arr[i][0]
-        arrEndIdx = i
+result = 0
+idx, val = arr[0][0], arr[0][1]
+for i in range(1, arrMaxIdx+1):
+    if val < arr[i][1]:
+        result += (arr[i][0] - idx) * val
+        idx, val = arr[i][0], arr[i][1]
 
-    if startIdx > arr[i][0]:
-        startIdx = arr[i][0]
+idx, val = arr[-1][0], arr[-1][1]
+for i in range(N-2, arrMaxIdx-1, -1):
+    if val < arr[i][1]:
+        result += (idx - arr[i][0]) * val
+        idx, val = arr[i][0], arr[i][1]
 
-resultArr = []
-val, idx = 0, startIdx
-for j in range(startIdx, maxIdx + 1):
-    for k in range(N):
-        if arr[k][0] == j and val < arr[k][1]:
-            val = arr[k][1]
-    resultArr.append(val)
+result += (idx - arr[arrMaxIdx][0] + 1) * maxVal
 
-val = arr[arrEndIdx][1]
-resultArr2 = []
-for j in range(endIdx, maxIdx, -1):
-    for k in range(N):
-        if arr[k][0] == j and arr[k][1] <= maxVal and arr[k][1] > val:
-            val = arr[k][1]
-    resultArr2.append(val)
-
-resultArr2.reverse()
-print(sum(resultArr+resultArr2))
+print(result)
